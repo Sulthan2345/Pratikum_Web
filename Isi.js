6313d5c6271031d17b054a3e4e1b927af85e77da
Sulthan2345 <166530755+Sulthan2345@users.noreply.github.com>
2024-10-10 00:58:04 +0700

    Update Isi.js

--- Isi.js
+++ Isi.js
@@ -3,5 +3,5 @@ document.writeln("<hr width=600 color=Black>");
 document.writeln("<h1>UNIVERSITAS LAMPUNG</h1>");
 document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</h2>");
 document.writeln("<h3>JURUSAN PENDIDIKAN DAN ILMU PENGETAHUAN ALAM</h3>");
-document.writeln("<hr width=600 color=Black>");
+document.writeln("<hr width=800 color=Black>");
 document.writeln("</center>");
